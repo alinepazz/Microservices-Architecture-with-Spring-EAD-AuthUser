ba129010e9fd6e87584e5c73d556c6ec076f2a1b
Aline Paz <aline.soares.2@hotmail.com>
2023-11-21 14:59:23 -0300

    Implementar Validation Tokens e Authorization com JWT

--- ead-authuser/src/main/java/com/ead/authuser/configs/security/WebSecurityConfig.java
+++ ead-authuser/src/main/java/com/ead/authuser/configs/security/WebSecurityConfig.java
@@ -10,8 +10,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -26,20 +28,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String [] AUTH_WHITELIST = {
             "/auth/**"
     };
-
+    @Bean
+    public AuthenticationJwtFilter authenticationJwtFilter(){
+        return new AuthenticationJwtFilter();
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http
-                .httpBasic()
+                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
                 .authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
                 .antMatchers(AUTH_WHITELIST).permitAll()
-                .antMatchers(HttpMethod.GET, "/users/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable()
                 .formLogin();
+        http.addFilterBefore(authenticationJwtFilter(), UsernamePasswordAuthenticationFilter.class);
     }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
