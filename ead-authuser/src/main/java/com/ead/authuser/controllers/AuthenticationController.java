4cacc684ae0488a0044d368ad0d1f184db8c0697
Aline Paz <aline.soares.2@hotmail.com>
2024-02-02 19:01:57 -0300

    Propagação de PaymentEvent pela Arquitetura

--- ead-authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ ead-authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -61,14 +61,15 @@ public class AuthenticationController {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email is Already taken!");
         }
 
-        RoleModel roleModel = roleService.findByRoleName(RoleType.ROLE_STUDENT).orElseThrow(()
+        RoleModel roleModel = roleService.findByRoleName(RoleType.ROLE_USER)
+                .orElseThrow(()
         -> new RuntimeException("Error: Role is not found"));
 
         userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
         var userModel = new UserModel();
         BeanUtils.copyProperties(userDto, userModel);
         userModel.setUserStatus(UserStatus.ACTIVE);
-        userModel.setUserType(UserType.STUDENT);
+        userModel.setUserType(UserType.USER);
         userModel.getRoles().add(roleModel);
         userService.saveUser(userModel);
         log.debug("POST registerUser - userId saved {} ", userModel.getUserId());
